51deafe7774a25db49110f3b11d7ec9ebc39b78f
blake-nx <benloe@team.nxlink.com>
2023-08-01 19:33:47 -0500

    removed old template code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,35 @@
-import { CodeBlock } from '@/components/CodeBlock';
-import { LanguageSelect } from '@/components/LanguageSelect';
-import { ModelSelect } from '@/components/ModelSelect';
-import { TextBlock } from '@/components/TextBlock';
-import { OpenAIModel, GenerateBody } from '@/types/types';
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { CodeBlock } from "@/components/CodeBlock";
+import { LanguageSelect } from "@/components/LanguageSelect";
+import { ModelSelect } from "@/components/ModelSelect";
+import { OpenAIModel, GenerateBody } from "@/types/types";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [inputLanguage, setInputLanguage] = useState<string>('JavaScript');
-  const [inputCode, setInputCode] = useState<string>('');
-  const [outputCode, setOutputCode] = useState<string>('');
-  const [model, setModel] = useState<OpenAIModel>('gpt-4');
+  const [inputLanguage, setInputLanguage] = useState<string>("JavaScript");
+  const [inputCode, setInputCode] = useState<string>("");
+  const [outputCode, setOutputCode] = useState<string>("");
+  const [model, setModel] = useState<OpenAIModel>("gpt-4");
   const [loading, setLoading] = useState<boolean>(false);
   const [hasGenerated, sethasGenerated] = useState<boolean>(false);
 
   const handleGenerate = async () => {
-    const maxCodeLength = model === 'gpt-3.5-turbo-16k' ? 64000 : 32000;
+    const maxCodeLength = model === "gpt-3.5-turbo-16k" ? 64000 : 32000;
 
     if (!inputCode) {
-      alert('Please enter some code.');
+      alert("Please enter some code.");
       return;
     }
 
     if (inputCode.length > maxCodeLength) {
       alert(
-        `Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`,
+        `Please enter code less than ${maxCodeLength} characters. You are currently at ${inputCode.length} characters.`
       );
       return;
     }
 
     setLoading(true);
-    setOutputCode('');
+    setOutputCode("");
 
     const controller = new AbortController();
 
@@ -40,10 +39,10 @@ export default function Home() {
       model,
     };
 
-    const response = await fetch('/api/generate', {
-      method: 'POST',
+    const response = await fetch("/api/generate", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       signal: controller.signal,
       body: JSON.stringify(body),
@@ -51,7 +50,7 @@ export default function Home() {
 
     if (!response.ok) {
       setLoading(false);
-      alert('Something went wrong.');
+      alert("Something went wrong.");
       return;
     }
 
@@ -59,14 +58,14 @@ export default function Home() {
 
     if (!data) {
       setLoading(false);
-      alert('Something went wrong.');
+      alert("Something went wrong.");
       return;
     }
 
     const reader = data.getReader();
     const decoder = new TextDecoder();
     let done = false;
-    let code = '';
+    let code = "";
 
     while (!done) {
       const { value, done: doneReading } = await reader.read();
@@ -84,11 +83,11 @@ export default function Home() {
   };
 
   const copyToClipboard = (text: string) => {
-    const el = document.createElement('textarea');
+    const el = document.createElement("textarea");
     el.value = text;
     document.body.appendChild(el);
     el.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(el);
   };
 
@@ -98,16 +97,11 @@ export default function Home() {
     }
   }, []);
 
-
-
   return (
     <>
       <Head>
         <title>Unit Test Generator</title>
-        <meta
-          name="description"
-          content="Use AI to generate Unit Tests."
-        />
+        <meta name="description" content="Use AI to generate Unit Tests." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -115,28 +109,28 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center mt-10 sm:mt-20">
           <div className="text-4xl font-bold">Unit Test Generator</div>
         </div>
-        
+
         <div className="flex items-center mt-2 space-x-2">
-          If your code is too long, you can switch to the GPT-3.5 model which has a limit of 16k tokens.
+          If your code is too long, you can switch to the GPT-3.5 model which
+          has a limit of 16k tokens.
         </div>
-        
+
         <div className="mt-2 text-xs text-center">
           {loading
-            ? 'Doing the thing...'
+            ? "Doing the thing..."
             : hasGenerated
-            ? 'Output copied to clipboard!'
+            ? "Output copied to clipboard!"
             : 'Enter your code, select your language, and click "Generate"'}
-
         </div>
-        
-         <div className="flex items-center mt-2 space-x-2">
+
+        <div className="flex items-center mt-2 space-x-2">
           <ModelSelect model={model} onChange={(value) => setModel(value)} />
-                                  <button
+          <button
             className="w-[140px] cursor-pointer rounded-md bg-violet-500 px-4 py-2 font-bold hover:bg-violet-600 active:bg-violet-700"
             onClick={() => handleGenerate()}
             disabled={loading}
           >
-            {loading ? 'Generating...' : 'Generate'}
+            {loading ? "Generating..." : "Generate"}
           </button>
         </div>
         <div className="mt-6 flex w-full max-w-[1200px] flex-col justify-between sm:flex-row sm:space-x-4">
@@ -146,37 +140,26 @@ export default function Home() {
               onChange={(value) => {
                 setInputLanguage(value);
                 sethasGenerated(false);
-                setInputCode('');
-                setOutputCode('');
+                setInputCode("");
+                setOutputCode("");
               }}
             />
-
-            {inputLanguage === 'Natural Language' ? (
-              <TextBlock
-                text={inputCode}
-                editable={!loading}
-                onChange={(value) => {
-                  setInputCode(value);
-                  sethasGenerated(false);
-                }}
-              />
-            ) : (
-              <CodeBlock
-                code={inputCode}
-                editable={!loading}
-                onChange={(value) => {
-                  setInputCode(value);
-                  sethasGenerated(false);
-                }}
-              />
-            )}
+            <CodeBlock
+              code={inputCode}
+              editable={!loading}
+              onChange={(value) => {
+                setInputCode(value);
+                sethasGenerated(false);
+              }}
+            />
           </div>
           <div className="flex flex-col justify-center h-full mt-8 space-y-2 sm:mt-0 sm:w-2/4">
-            <div className="w-full rounded-md bg-[#1F2937] px-4 py-2 text-neutral-200 font-bold text-center">Output</div>
-              <CodeBlock code={outputCode} />
+            <div className="w-full rounded-md bg-[#1F2937] px-4 py-2 text-neutral-200 font-bold text-center">
+              Output
+            </div>
+            <CodeBlock code={outputCode} />
           </div>
         </div>
-
       </div>
     </>
   );
--- types/types.ts
+++ types/types.ts
@@ -1,11 +1,7 @@
-export type OpenAIModel = 'gpt-3.5-turbo-16k' | 'gpt-4';
+export type OpenAIModel = "gpt-3.5-turbo-16k" | "gpt-4";
 
 export interface GenerateBody {
   inputLanguage: string;
   inputCode: string;
   model: OpenAIModel;
-}
-
-export interface TranslateResponse {
-  code: string;
 }
